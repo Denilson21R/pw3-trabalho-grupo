7709c24c65b227b16621666e9f3bb45c6752eda1
Denilson21R <45276744+Denilson21R@users.noreply.github.com>
2022-12-08 19:47:17 -0300

    get all receitas

--- frontend/src/app/components/receitas/receitas.component.ts
+++ frontend/src/app/components/receitas/receitas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Receita} from "../../model/Receita";
+import {WebService} from "../../../web.service";
 
 @Component({
   selector: 'app-receitas',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receitas.component.scss']
 })
 export class ReceitasComponent implements OnInit {
+  receitas!: Receita[]
 
-  constructor() { }
+  constructor(private web: WebService) { }
 
   ngOnInit(): void {
+    this.fillReceitas();
+  }
+
+  private fillReceitas() {
+    this.web.getAllReceitas().subscribe((response) => {
+      if (response.ok) {
+        this.receitas = response.body!
+      } else {
+        alert("Ocorreu um erro ao obter as receitas");
+      }
+    })
   }
 
 }
